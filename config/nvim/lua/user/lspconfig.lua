aa83201e1cb9ee201ad6c702e38d28d8fcef0522
sajenim <its.jassy@pm.me>
2023-04-22 10:41:56 +0800

    diagnostic enable/disable mappings

--- config/nvim/lua/user/lspconfig.lua
+++ config/nvim/lua/user/lspconfig.lua
@@ -1,5 +1,7 @@
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>de', vim.diagnostic.enable)
+vim.keymap.set('n', '<space>dd', vim.diagnostic.disable)
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
