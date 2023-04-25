29560e8589f9d8737994752e749fe12ce13b0a3d
sajenim <its.jassy@pm.me>
2023-04-25 08:13:04 +0800

    add candybar package

---
+++ pkgs/candybar/default.nix
@@ -0,0 +1,9 @@
+{ pkgs }:
+
+pkgs.haskellPackages.developPackage {
+  root = ./.;
+  modifier = drv:
+    pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+      [ cabal-install ]
+    );
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@
 
 { pkgs ? (import ../nixpkgs.nix) { } }: {
   # example = pkgs.callPackage ./example { };
+  candybar = pkgs.callPackage ./candybar { };
 }
