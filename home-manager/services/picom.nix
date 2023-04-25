9b62c6fdd794691298b75a3d8906f52f95dd36da
sajenim <its.jassy@pm.me>
2023-04-25 08:06:20 +0800

    fix rounded borders

--- home-manager/services/picom.nix
+++ home-manager/services/picom.nix
@@ -4,10 +4,12 @@
   services.picom = {
     enable = true;
     shadow = true;
+    backend = "glx";
     settings = {
-      corner-radius = 5;
+      corner-radius = 10;
       rounded-corners-exclude = [
-        "window_type = 'dock'"
+        # "window_type = 'normal'"
+        # "window_type = 'dock'"
       ];
     };
   };
