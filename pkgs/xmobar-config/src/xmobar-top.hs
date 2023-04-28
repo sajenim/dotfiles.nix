527c4db1afd1a4ecb015243be67b4e7e51f45097
sajenim <its.jassy@pm.me>
2023-04-28 20:46:53 +0800

    basic bar

--- pkgs/xmobar-config/src/xmobar-top.hs
+++ pkgs/xmobar-config/src/xmobar-top.hs
@@ -1,30 +1,47 @@
 import Xmobar
-import System.Environment (getEnv)
-import System.IO.Unsafe   (unsafeDupablePerformIO)
 
--- | Candybar
+-- | Configuration
 config :: Config
 config = defaultConfig
   -- general settings
   { font            = "Fira Code Semi Bold 9"
-  , additionalFonts = [ ]
-  , bgColor         = "#282828"
-  , fgColor         = "#d4be98"
+  , additionalFonts = [ "Symbols Nerd Font 2048-em 24" ]
+  , bgColor         = bg1
+  , fgColor         = fg0
   , position        = Static { xpos = 1920, ypos = 0, width = 2560, height = 24 }
-  , iconRoot        = homeDir <> "/.config/xmonad/icons"
   -- commands to run
-  , commands = 
-      [ Run $ XMonadLog
+  , commands =
+      [ Run $ XPropertyLog "_XMONAD_LOG_1"
+      , Run $ Com "uname" ["-r","-s"] "" 0
+      , Run $ Date "%a %b %_d %Y <fc=#d8a657>%H:%M:%S</fc>" "date" 10
+      , Run $ Weather "YPJT"
+                      [ "-t", "Temp: <fc=#d3869b><tempC>C</fc> <fc=#7c6f64>|</fc> Wind: <fc=#a9b665><windKmh>km/h</fc> <fc=#7c6f64>|</fc> Humidity: <fc=#e78a4e><rh>%</fc>" ]
+                      18000
       ]
   -- format our bar
   , sepChar  = "%"
   , alignSep = "}{"
-  , template = "\59255 %XMonadLog%"
+  , template = "<hspace=6/><fn=1>\59255</fn>  <fc=#a9b665>%uname%</fc> <fc=#7c6f64>|</fc>%_XMONAD_LOG_1%}{%YPJT% <fc=#7c6f64>|</fc> %date%<hspace=6/>"
   }
 
--- | Get home directory
-homeDir :: String
-homeDir = unsafeDupablePerformIO (getEnv "HOME")
+-- | Gruvbox material
+bg0, bg1, fg0, fg1, red, orange, yellow, green, aqua, blue, purple :: String
+-- backgrounds
+bg0    = "#1d2021"
+bg1    = "#282828"
+-- foregrounds
+fg0    = "#d4be98"
+fg1    = "#ddc7a1"
+-- greys
+grey0  = "#7c6f64"
+-- colors
+red    = "#ea6962"
+orange = "#e78a4e"
+yellow = "#d8a658"
+green  = "#a9b665"
+aqua   = "#89b482"
+blue   = "#7daea3"
+purple = "#d3869b"
 
 main :: IO ()
 main = configFromArgs config >>= xmobar
