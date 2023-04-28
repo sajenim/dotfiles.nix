ab59f661fc242ca7f06e46ac3c872002b9f5a117
sajenim <its.jassy@pm.me>
2023-04-28 13:49:41 +0800

    update channels + follow unstable

--- flake.nix
+++ flake.nix
@@ -2,19 +2,20 @@
   description = "NixOS + Home Manager configuration with flakes";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
-
+    # Nixpkgs branches
+    master.url = "github:nixos/nixpkgs/master";
+    stable.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/master";
 
     # Add any other flake you might need
     nixpkgs-f2k.url = "github:fortuneteller2k/nixpkgs-f2k";
+
+    # Default branch
+    nixpkgs.follows = "nixos-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
