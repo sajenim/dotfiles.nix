0ee94132d470abaa6fd79de846d4b4a5f473934a
sajenim <its.jassy@pm.me>
2023-06-09 23:16:42 +0800

    remove bitwarden container

--- nixos/lavender/containers/default.nix
+++ nixos/lavender/containers/default.nix
@@ -2,30 +2,16 @@
 
 {
   imports = [
-    # Dashboard
     ./homepage
-
-    # Website
     ./nginx
-
-    # Dns blackhole
     ./pihole
-
-    # Multimedia
     ./plex
     ./sonarr
     ./radarr
     ./prowlarr
     ./recyclarr
-
-    # Documents & Files
     ./qbittorrent
-    ./bitwarden
-
-    # Game servers
     ./minecraft
-
-    # Reverse proxy
     ./traefik
   ];
 }
