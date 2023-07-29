2b9c3b7b4c2979cc120940358baf3622dd3d4caf
sajenim <its.jassy@pm.me>
2023-07-29 08:30:44 +0800

    enable host networking + docker.sock

--- nixos/lavender/containers/homepage/default.nix
+++ nixos/lavender/containers/homepage/default.nix
@@ -8,7 +8,9 @@
     volumes = [
       "/srv/containers/homepage:/app/config"
       "/srv/data:/srv/data:ro"
+      "/var/run/docker.sock:/var/run/docker.sock" # pass local proxy
     ];
     ports = [ "3000:3000" ];
+    extraOptions = ["--network=host"];
   };
 }
--- nixos/lavender/containers/traefik/default.nix
+++ nixos/lavender/containers/traefik/default.nix
@@ -20,5 +20,6 @@
       CF_API_EMAIL_FILE = "/secrets/cf-api-email";
       CF_API_KEY_FILE = "/secrets/cf-api-key";
     };
+    extraOptions = ["--network=host"];
   };
 }
