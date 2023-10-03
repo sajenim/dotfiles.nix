c9cfd8b9dfe7d27f1981aeb1168875d79a7fc425
sajenim <its.jassy@pm.me>
2023-10-03 14:59:56 +0800

    configure mangohud

--- home-manager/sajenim/programs/mangohud/default.nix
+++ home-manager/sajenim/programs/mangohud/default.nix
@@ -3,12 +3,30 @@
 {
   programs.mangohud = {
     enable = true;
+    package = pkgs.unstable.mangohud;
     settings = {
+      # GPU
       gpu_temp = true;
+      gpu_junction_temp = true;
       gpu_core_clock = true;
+      gpu_fan = true;
+      gpu_voltage = true;
+
+      # CPU
       cpu_temp = true;
       cpu_mhz = true;
-      toggle_hud = "F12";
+
+      # FPS
+      fps = true;
+      frametime = false;
+      frame_timing = false;
+
+      # Miscellaneous
+      wine = true;
+      gamemode = true;
+
+      # Hud dimensions
+      table_columns = 4;
     };
   };
 }
