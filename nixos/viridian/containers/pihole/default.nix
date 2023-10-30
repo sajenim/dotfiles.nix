59883557ed9287e6b921f4ad982c8e7feaf59450
sajenim <its.jassy@pm.me>
2023-10-30 21:38:56 +0000

    fix local ip

--- nixos/viridian/containers/pihole/default.nix
+++ nixos/viridian/containers/pihole/default.nix
@@ -11,9 +11,9 @@
       "/srv/containers/pihole/secrets:/secrets"
     ];
     ports = [ 
-      "192.168.1.100:53:53/tcp"   # pihole-FTL (DNS)
-      "192.168.1.100:53:53/udp"   # pihole-FTL (DNS)
-      "192.168.1.100:8181:80/tcp" # lighttpd   (HTTP)
+      "192.168.1.102:53:53/tcp"   # pihole-FTL (DNS)
+      "192.168.1.102:53:53/udp"   # pihole-FTL (DNS)
+      "192.168.1.102:8181:80/tcp" # lighttpd   (HTTP)
     ];
     environment = {
       WEBPASSWORD_FILE = "/secrets/admin-password";
