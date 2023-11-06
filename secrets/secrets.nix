090022c51fca03976bf1545686e152937a17c976
sajenim <its.jassy@pm.me>
2023-11-06 18:12:55 +0800

    update secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,17 +1,18 @@
 let
   # Users
+  sajenim = {
+    fuchsia = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIN6zrgWQj9NbuZEvqK3r2U9jSJjLotGb8asUkcr+co5sAAAABHNzaDo=";
+  };
+
   sabrina = {
     viridian = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPn1X4oHniIkR1bEP/KFt70gmtA3tc8UTLd53P9xQzNe";
-  };
-  erika = {
-    fuchsia = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEu9QzwNxSReeVkr/8ChHfwValDjZ1xYiPHP/E04n5c3AAAABHNzaDo=";
   };
   
   # Hosts
   viridian = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINR3TagUNIDTEPkVVBl6Pot+6GocrQ5/2Dq72RRiQWqe";
   fuchsia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4L6G8YIDE2Ej02lnQ6KSU5B011VO5kR9UBnVNcBJsg";
   
-  users = [ sabrina.viridian erika.fuchsia];
+  users = [ sajenim.fuchsia sabrina.viridian ];
   hosts = [ viridian fuchsia];
 in
   {
