f88180895e22bf83f6fffcb3f094ca7e3f5c5ffa
sajenim <its.jassy@pm.me>
2023-11-18 13:04:09 +0000

    setup homarr (dashboard)

---
+++ nixos/viridian/containers/dashboard/default.nix
@@ -0,0 +1,24 @@
+{ ... }:
+
+{
+  virtualisation.oci-containers.containers = {
+    dashboard = {
+      autoStart = true;
+      image = "ghcr.io/ajnart/homarr:latest";
+      ports = [
+        "7575:7575/tcp" # WebUI
+      ];
+      volumes = [
+        # Container data
+        "/var/lib/homarr/configs:/app/data/configs:rw"
+        "/var/lib/homarr/icons:/app/public/icons:rw"
+        "/var/lib/homarr/data:/data:rw"
+        # Docker Integration
+        "/var/run/docker.sock:/var/run/docker.sock:ro"
+      ];
+      extraOptions = [
+        "--network=host"
+      ];
+    };
+  };
+}
