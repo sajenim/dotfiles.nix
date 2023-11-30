67aa524fa4e46855b9effb51b6fb3ca670e70da7
sajenim <its.jassy@pm.me>
2023-12-01 05:48:50 +0800

    update ssh keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,14 @@
 let
   # Users
-  sabrina = {
-    viridian = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPn1X4oHniIkR1bEP/KFt70gmtA3tc8UTLd53P9xQzNe";
-  };
+  sabrina = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPn1X4oHniIkR1bEP/KFt70gmtA3tc8UTLd53P9xQzNe";
+  sajenim = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAXk3P0EcVnSnWnll+m5Vv/s3QyiC73ERNLJmgxnRE1q";
   
   # Hosts
   viridian = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINR3TagUNIDTEPkVVBl6Pot+6GocrQ5/2Dq72RRiQWqe";
-  fuchsia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4L6G8YIDE2Ej02lnQ6KSU5B011VO5kR9UBnVNcBJsg";
+  fuchsia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/NsyclB3T7zJ7N8Mi+cxtPH1jzG7jEn3QK33rO0RgQ";
   
-  users = [ sabrina.viridian ];
-  hosts = [ viridian fuchsia];
+  users = [ sabrina sajenim ];
+  hosts = [ viridian fuchsia ];
 in
   {
     "traefik.age".publicKeys = users ++ hosts;
