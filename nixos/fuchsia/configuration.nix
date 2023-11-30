eed97c13a2d9dfe22b0381b781bb6318d4b86e39
sajenim <its.jassy@pm.me>
2023-12-01 05:52:42 +0800

    enable sshd

--- nixos/fuchsia/configuration.nix
+++ nixos/fuchsia/configuration.nix
@@ -191,6 +191,16 @@
   };
 
   services = {
+    # This setups a SSH server. Very important if you're setting up a headless system.
+    # Feel free to remove if you don't need it.
+    openssh = {
+      enable = true;
+      # Forbid root login through SSH.
+      settings.PermitRootLogin = "no";
+      # Use keys only. Remove if you want to SSH using password (not recommended)
+      settings.PasswordAuthentication = false;
+    };
+
     udev.packages = with pkgs; [
       yubikey-personalization
       openrgb
