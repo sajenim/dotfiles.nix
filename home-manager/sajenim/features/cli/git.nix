44413bea217f9fc93c4016d1c585df53db024f37
sajenim <its.jassy@pm.me>
2024-01-21 23:12:08 +0800

    fix keys

--- home-manager/sajenim/features/cli/git.nix
+++ home-manager/sajenim/features/cli/git.nix
@@ -11,8 +11,8 @@
     userEmail = "its.jassy@pm.me";
     extraConfig = {
       init.defaultBranch = "main";
-      core.sshCommand = "~/.ssh/sajenim_sk";
-      user.signingkey = "~/.ssh/signing_sk";
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/signing_sk.pub";
     };
   };
 }
