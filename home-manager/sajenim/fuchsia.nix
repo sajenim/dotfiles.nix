17d302cbdfdeae6397e8849eddb71f5531b5eae6
sajenim <its.jassy@pm.me>
2024-01-22 20:00:20 +0800

    refactor

--- home-manager/sajenim/fuchsia.nix
+++ home-manager/sajenim/fuchsia.nix
@@ -13,27 +13,34 @@
 
   home = {
     packages = with pkgs; [
-      spotify
-      jellyfin-media-player
-      firefox
-      pulsemixer
       # Graphics
       gimp
       inkscape
       krita
+
       # Games
       gamemode
       protonup-ng
       prismlauncher
       runelite
+
       # Hardware
       libratbag
       piper
+
+      # Misc
+      spotify
+      jellyfin-media-player
+      firefox
+      pulsemixer
     ];
+
     persistence."/persist/home/sajenim" = {
       directories = [
         ".mozilla"
+
         ".config/Yubico"
+
         "Documents"
         "Downloads"
         "Games"
