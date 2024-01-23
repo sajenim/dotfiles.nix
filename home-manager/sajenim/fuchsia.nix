34c6ba9daa46b98dee452acd418942a06bc38c66
sajenim <its.jassy@pm.me>
2024-01-23 09:46:37 +0800

    persist prismlauncher, add comments

--- home-manager/sajenim/fuchsia.nix
+++ home-manager/sajenim/fuchsia.nix
@@ -17,17 +17,14 @@
       gimp
       inkscape
       krita
-
       # Games
       gamemode
       protonup-ng
       prismlauncher
       runelite
-
       # Hardware
       libratbag
       piper
-
       # Misc
       spotify
       jellyfin-media-player
@@ -38,9 +35,11 @@
     persistence."/persist/home/sajenim" = {
       directories = [
         ".mozilla"
-
+        # Mutable configurations
         ".config/Yubico"
-
+        # Application specific data
+        ".local/share/PrismLauncher"
+        # Our user data
         "Documents"
         "Downloads"
         "Games"
