b6fc2bf19c4f07f6376e504f8ed583d747e957be
sajenim <its.jassy@pm.me>
2024-01-23 09:48:12 +0800

    backup containers

--- nixos/viridian/services/borgbackup.nix
+++ nixos/viridian/services/borgbackup.nix
@@ -2,36 +2,12 @@
 
 {
   services.borgbackup.jobs = {
-    multimedia = {
+    containers = {
       paths = [
-        "/var/lib/jellyfin"
-        "/var/lib/jellyseerr"
-        "/var/lib/sonarr"
-        "/var/lib/radarr"
-        "/var/lib/lidarr"
+        "/srv/containers"
       ];
       encryption.mode = "none";
-      repo = "/mnt/backup/multimedia/borg";
-      compression = "auto,zstd";
-      startAt = "daily";
-    };
-
-    torrents = {
-      paths = [
-        "/var/lib/qbittorrent"
-      ];
-      encryption.mode = "none";
-      repo = "/mnt/backup/torrents/borg";
-      compression = "auto,zstd";
-      startAt = "daily";
-    };
-
-    minecraft = {
-      paths = [
-        "/var/lib/minecraft"
-      ];
-      encryption.mode = "none";
-      repo = "/mnt/backup/minecraft/borg";
+      repo = "/srv/backup/borg/containers";
       compression = "auto,zstd";
       startAt = "daily";
     };
