10e4eed61d0b1d9e3e353789dd74a4bd1c89b624
sajenim <its.jassy@pm.me>
2024-02-13 22:53:59 +0800

    remove unused pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,5 +4,4 @@
 { pkgs ? (import ../nixpkgs.nix) { } }: {
   xmobar = pkgs.callPackage ./xmobar-config { };
   xmonad = pkgs.callPackage ./xmonad-config { };
-  jellyfin-rpc = pkgs.callPackage ./jellyfin-rpc { };
 }
