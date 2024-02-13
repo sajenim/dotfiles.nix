6a3e6044449a719beb98587e07971987a74936c6
sajenim <its.jassy@pm.me>
2024-02-13 22:58:20 +0800

    pulseaudio fix for mpd

--- nixos/fuchsia/configuration.nix
+++ nixos/fuchsia/configuration.nix
@@ -28,6 +28,7 @@
     pulseaudio = {
       enable = true;
       support32Bit = true;
+      extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
     };
     opengl = {
       enable = true;
