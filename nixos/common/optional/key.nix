4c3f590364ac2ce678721068da094e407560c3aa
sajenim <its.jassy@pm.me>
2024-04-14 21:57:21 +0800

    yubikey gpg/cryptsetup

--- nixos/common/optional/key.nix
+++ nixos/common/optional/key.nix
@@ -2,10 +2,14 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # Configure your YubiKey via the command line
-    yubikey-manager
     # Enables files to be encrypted to age identities stored on YubiKeys
     age-plugin-yubikey
+    # Cryptfile
+    cryptsetup
+    # Yubikey can be used as a smart card for secure encryption
+    gnupg
+    # Configure your YubiKey via the command line
+    yubikey-manager
   ];
 
   # Use our yubikey as a user login or for sudo access 
@@ -13,4 +17,7 @@
     login.u2fAuth = true;
     sudo.u2fAuth = true;
   };
+
+  # Enable udev rules for gnupg smart cards
+  hardware.gpgSmartcards.enable = true;
 }
