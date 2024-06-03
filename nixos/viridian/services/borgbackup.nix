8fc345ca49359196e8616b85eeacc626e57f8662
jasmine <its.jassy@pm.me>
2024-06-03 21:20:24 +0800

    Update our backup strategy

--- nixos/viridian/services/borgbackup.nix
+++ nixos/viridian/services/borgbackup.nix
@@ -7,7 +7,21 @@
         "/srv/containers"
       ];
       encryption.mode = "none";
-      repo = "/srv/backup/borg/containers";
+      repo = "/srv/backup/containers";
+      compression = "auto,zstd";
+      startAt = "daily";
+    };
+
+    services = {
+      paths = [
+        "/srv/services"
+      ];
+      encryption.mode = "none";
+      repo = "/srv/backup/services";
+      compression = "auto,zstd";
+      startAt = "daily";
+    };
+
     shares = {
       paths = [
         "/srv/shares"
