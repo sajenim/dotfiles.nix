83990a7d428dec26e42d2e96b24abe3a95167b44
jasmine <its.jassy@pm.me>
2024-07-21 19:34:08 +0800

    Remove depricated plugin

--- home-manager/sajenim/features/nvim/config/lua/plugins/editor.lua
+++ home-manager/sajenim/features/nvim/config/lua/plugins/editor.lua
@@ -82,19 +82,6 @@ return {
     opts = { },
   },
 
-  -- 
-  -- Preview Markdown
-  --
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function ()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
-
   --
   -- Delete Buffers
   --
