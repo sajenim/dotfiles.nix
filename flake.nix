815bc493ad7838dd6bbb112d59695e0634d85fff
jasmine <its.jassy@pm.me>
2024-08-05 08:48:06 +0800

    Update flake boilerplate

--- flake.nix
+++ flake.nix
@@ -34,27 +34,25 @@
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       inherit (self) outputs;
-      forAllSystems = nixpkgs.lib.genAttrs [
+      # Supported systems for your flake packages, shell, etc.
+      systems = [
         "aarch64-linux"
         "i686-linux"
         "x86_64-linux"
         "aarch64-darwin"
         "x86_64-darwin"
       ];
+      # This is a function that generates an attribute by calling a function you
+      # pass to it, with each system as an argument
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
       # Your custom packages
       # Acessible through 'nix build', 'nix shell', etc
-      packages = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./pkgs { inherit pkgs; }
-      );
-      # Devshell for bootstrapping
-      # Acessible through 'nix develop' or 'nix-shell' (legacy)
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./shell.nix { inherit pkgs; }
-      );
+      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      # Formatter for your nix files, available through 'nix fmt'
+      # Other options beside 'alejandra' include 'nixpkgs-fmt'
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
       # Your custom packages and modifications, exported as overlays
       overlays = import ./overlays { inherit inputs; };
@@ -90,6 +88,8 @@
         };
       };
 
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
       homeConfigurations = {
         "sajenim@fuchsia" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,7 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
+# You can build them using 'nix build .#example'
 
-{ pkgs ? (import ../nixpkgs.nix) { } }: {
+pkgs: {
   xmobar = pkgs.callPackage ./xmobar-config { };
   xmonad = pkgs.callPackage ./xmonad-config { };
   amdgpu-clocks = pkgs.callPackage ./amdgpu-clocks { };
