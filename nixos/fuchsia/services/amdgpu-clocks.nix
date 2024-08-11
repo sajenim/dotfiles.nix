802e1c528992d7c25bbf4491a218100e95af15c1
jasmine <its.jassy@pm.me>
2024-08-11 08:08:11 +0800

    update: amdgpu-clocks

--- nixos/fuchsia/services/amdgpu-clocks.nix
+++ nixos/fuchsia/services/amdgpu-clocks.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   # Our custom power state
   environment.etc = {
-    "default/amdgpu-custom-states.card0" = {
+    "default/amdgpu-custom-states.card1" = {
       text = ''
         # For Navi (and Radeon7) we can only set highest SCLK & MCLK, "state 1":
         OD_SCLK:
@@ -10,7 +10,7 @@
         1: 875MHz
         # More fine-grain control of clocks and voltages are done with VDDC curve:
         OD_VDDC_CURVE:
-        0: 800MHz @ 699mV
+        0: 800MHz @ 698mV
         1: 1450MHz @ 795mV
         2: 1800MHz @ 950mV
         # Force power limit (in micro watts):
