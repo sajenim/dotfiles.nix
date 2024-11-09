a1d5b5a23b57013355d423b3db6117566508915f
jasmine <its.jassy@pm.me>
2024-11-09 08:11:53 +0800

    remove some unstable packages

--- home-manager/sajenim/features/printing/default.nix
+++ home-manager/sajenim/features/printing/default.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
   home = {
     packages = with pkgs; [
-      unstable.blender
-      freecad # https://discourse.nixos.org/t/error-hash-mismatch-in-fixed-output-derivation/52353
-      unstable.kicad
-      unstable.openscad
-      prusa-slicer # Some error idk fix this later
+      blender
+      freecad
+      kicad
+      openscad
+      prusa-slicer
     ];
   };
 }
