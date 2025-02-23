3281960408ea4f56ef6804998deb17fcc1f2f4f1
jasmine <its.jassy@pm.me>
2025-02-23 20:14:55 +0800

    feat: remove round borders

--- home-manager/sajenim/features/desktop/picom/default.nix
+++ home-manager/sajenim/features/desktop/picom/default.nix
@@ -2,13 +2,6 @@
   services.picom = {
     enable = true;
     shadow = true;
-    backend = "glx";
-    settings = {
-      corner-radius = 10;
-      rounded-corners-exclude = [
-        "window_type = 'dock'"
-        "class_g = 'Rofi'"
-      ];
-    };
+    backend = "xrender";
   };
 }
