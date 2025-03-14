06c181c610eb606038af3b4df66a237711c9c230
jasmine <its.jassy@pm.me>
2025-03-14 21:43:26 +0800

    feat: add LLM models

--- nixos/fuchsia/services/ollama/default.nix
+++ nixos/fuchsia/services/ollama/default.nix
@@ -11,8 +11,12 @@
     # Language models to install
     loadModels = [
       "deepseek-coder-v2"
-      "llama3"
-      "mannix/llama3.1-8b-abliterated"
+      # these were dependencies for continue.dev
+      # but idk if i'll end up using it
+      # for now we shall keep them
+      "llama3.1:8b"
+      "qwen2.5-coder:1.5b-base"
+      "nomic-embed-text:latest"
     ];
   };
 }
